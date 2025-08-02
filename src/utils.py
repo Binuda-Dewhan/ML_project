77bf23000b3e0089bf175d83cc6a0e6cacb52fab
Binuda Dewhan <binudab4@gmail.com>
2025-08-02 23:28:41 +0530

    hyper parameter tunings and again train the model

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ from src.exception import CustomException
 from src.logger import logging
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     """
@@ -28,7 +29,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test , models):
+def evaluate_model(X_train,y_train,X_test,y_test , models,param):
     """
     Evaluates the performance of multiple models on the given dataset.
     
@@ -46,7 +47,13 @@ def evaluate_model(X_train,y_train,X_test,y_test , models):
         for i in range(len(list(models))):
 
             model = list(models.values())[i]
-            
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(estimator=model, param_grid=para, cv=3 , n_jobs=-1, verbose=1)
+            gs.fit(X_train, y_train)
+            # model.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
